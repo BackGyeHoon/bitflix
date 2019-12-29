523f35d0acc68534addc2ca5fcad23b195c119b2
백계훈 <gh.back@designgj.com>
2019-12-29 15:37:49 +0900

    add Header Components

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Router from 'Components/Router';
+import GlobalStyles from 'Components/GlobalStyles';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Router />
+        <GlobalStyles />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
